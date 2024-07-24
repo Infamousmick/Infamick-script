8d8fab41e62d7e5f02c81d9374ee53c1cbca90bf
Infamousmick <mmarchese6690@gmail.com>
2024-07-24 03:14:24 +0200

    Updates

--- customize.sh
+++ customize.sh
@@ -1,56 +1,93 @@
-#!/data/adb/magisk/busybox sh
-set -o standalone
+##########################################################################################
+#
+# Infamick Script Installer Script
+#
+##########################################################################################
 
-set -x
-# Check root environment
-VER=`grep_prop version $MODPATH/module.prop`
-VERCODE=`grep_prop versionCode $MODPATH/module.prop`
-ui_print "  ID: $MODID"
-ui_print "  Version: $VER"
-ui_print "  VersionCode: $VERCODE"
-if [ "$KSU" == true ]; then
-ui_print "  KSUVersion: $KSU_VER"
-ui_print "  KSUVersionCode: $KSU_VER_CODE"
-ui_print "  KSUKernelVersionCode: $KSU_KERNEL_VER_CODE"
-else
-ui_print "  MagiskVersion: $MAGISK_VER"
-ui_print "  MagiskVersionCode: $MAGISK_VER_CODE"
-fi
-ui_print " "
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=false
 
-# Check Android API
-[ $API -ge 23 ] ||
- abort "- Unsupported API version: $API"
+##########################################################################################
+# Installation Message
+##########################################################################################
 
-# Patch the XML and place the modified one to the original directory
-{
-NULL="/dev/null"
+print_modname() {
+  ui_print "*******************************"
+  ui_print "     Infamick Script v1.0      "
+  ui_print "*******************************"
 }
 
-# Additional add-on for check gms status
-ADDON() {
- ui_print "- Installing Tempcheck script"
-mkdir -p $MODPATH/system/bin
-mv -f $MODPATH/infamick $MODPATH/system/bin/infamick
+##########################################################################################
+# Changelog
+##########################################################################################
+
+show_changelog() {
+  ui_print " "
+  ui_print "Changelog for v1.0:"
+  ui_print "- Initial release of Infamick script"
+  ui_print "- Boot count reset functionality"
+  ui_print "- Temperature monitoring feature"
+  ui_print "- Battery health check"
+  ui_print "- Color-coded output for better readability"
+  ui_print " "
 }
 
-FINALIZE() {
- ui_print "- Finalizing installation"
+##########################################################################################
+# Replace list
+##########################################################################################
 
-# Clean up
- ui_print "  Cleaning obsolete files"
-find $MODPATH/* -maxdepth 0 \
-! -name 'module.prop' \
-! -name 'post-fs-data.sh' \
-! -name 'service.sh' \
-! -name 'system' \
--exec rm -rf {} \;
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
 
-# Settings dir and file permission
- ui_print "  Settings permissions"
-set_perm_recursive $MODPATH 0 0 0755 0755
-set_perm $MODPATH/system/bin/infamick 0 2000 0755
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here, it will override the example above
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+set_permissions() {
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Here are some examples for the set_perm functions:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
+
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
+  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
+  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+
+  # Set permissions for the script
+  set_perm  $MODPATH/system/bin/infamick  0  0  0755
 }
 
-# Final adjustment
-ADDON && FINALIZE
+##########################################################################################
+# Custom Functions
+##########################################################################################
+
+# This file (config.sh) will be sourced by the main flash script after util_functions.sh
+# If you need custom logic, please add them here as functions, and call these functions in
+# update-binary. Refrain from adding code directly into update-binary, as it will make it
+# difficult for you to migrate your modules to newer template versions.
+# Make update-binary as clean as possible, try to only do function calls in it.
+
+# Call the changelog function
+show_changelog
